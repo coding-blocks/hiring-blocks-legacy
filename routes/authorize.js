bcedc42763ae755836816f84f30f4356f7e093e8
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-06-17 01:02:45 +0530

    authorize

--- routes/authorize.js
+++ routes/authorize.js
@@ -1,21 +1,21 @@
-const router = require('express').Router();
+const route = require('express').Router();
 const models = require('./../db/models').models;
+const uid = require('uid2');
 
 
-router.post('/student', (req, res) => {
+route.post('/student', (req, res) => {
     password.pass2hash(req.body.password).then(function (hash) {
-
         models.StudentLocal.findOne({
             where: {
                 email: req.body.email,
                 password: hash
-            }
-            //TODO : Same as login
-        }).then(function (student) {
-            if (student) {
+            },
+            include: [models.Student]
+        }).then(function (studentLocal) {
+            if (studentLocal) {
                 models.AuthStudent.create({
                     token: uid(30),
-                    studentId: student.id
+                    studentId: studentLocal.student.id
                 }).then(function (authToken) {
                     res.send({
                         success: 'true',
@@ -36,5 +36,68 @@ router.post('/student', (req, res) => {
     })
 });
 
+route.post('/company', (req, res) => {
+    password.pass2hash(req.body.password).then(function (hash) {
+        models.CompanyLocal.findOne({
+            where: {
+                email: req.body.email,
+                password: hash
+            },
+            include: [models.Company]
+        }).then(function (companyLocal) {
+            if (companyLocal) {
+                models.AuthCompany.create({
+                    token: uid(30),
+                    companyId: companyLocal.company.id
+                }).then(function (authToken) {
+                    res.send({
+                        success: 'true',
+                        token: authToken.token
+                    })
+                }).catch(function (err) {
+                    console.log(err);
+                    res.send({success: 'false'})
+                })
+            }
+        }).catch(function (err) {
+            console.log(err);
+            res.send({success: 'false'})
+        })
+    }).catch(function (err) {
+        console.log(err);
+        res.send({success: 'error'});
+    })
+});
 
-module.exports = router;
+route.post('/admin', (req, res) => {
+    password.pass2hash(req.body.password).then(function (hash) {
+        models.Admin.findOne({
+            where: {
+                email: req.body.email,
+                password: hash
+            }
+        }).then(function (admin) {
+            if (admin) {
+                models.AuthStudent.create({
+                    token: uid(30),
+                    adminId: admin.id
+                }).then(function (authToken) {
+                    res.send({
+                        success: 'true',
+                        token: authToken.token
+                    })
+                }).catch(function (err) {
+                    console.log(err);
+                    res.send({success: 'false'})
+                })
+            }
+        }).catch(function (err) {
+            console.log(err);
+            res.send({success: 'false'})
+        })
+    }).catch(function (err) {
+        console.log(err);
+        res.send({success: 'error'});
+    })
+});
+module.exports = route;
