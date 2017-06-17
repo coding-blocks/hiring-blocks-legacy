bc6a3a0dd7be2dd3343675b80aac4f04a92ca618
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2017-06-17 12:42:04 +0530

    Update local strategies

--- auth/strategies/local/company.js
+++ auth/strategies/local/company.js
@@ -9,23 +9,28 @@ module.exports = new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password'
 }, function (email, password, done) {
-    passutils.pass2hash(password).then(function (hash) {
-        models.CompanyLocal.findOne({
-            where: {email: email},
-            include: models.Company
-        }).then(function (companylocal) {
-            if (!companylocal)
-                return done(null, false, {message: 'Incorrect email'});
-            if (companylocal.password === hash) {
-                return done(null, companylocal.company);
-            } else {
-                return done(null, false, {message: 'Incorrect password'});
-            }
-        }).catch(function (err) {
-            console.log(err);
-            return done(err, false, {message: 'invalid user'});
-        });
-    }).catch(function () {
+    models.CompanyLocal.findOne({
+        where: {email: email},
+        include: models.Company
+    }).then(function (companyLocal) {
+        console.log(companyLocal);
+        if (!companyLocal) {
+            return done(null, false, {message: 'Incorrect email'});
+        } else {
+            password.compare2hash(password, companyLocal.password).then(function (match) {
+                if (match) {
+                    console.log(2);
+                    return done(null, companyLocal.company);
+                } else {
+                    console.log(3);
+                    return done(null, false, {message: 'Incorrect password'});
+                }
+            }).catch(function (err) {
+                console.log(err);
+                return done(err, false, {message: 'invalid user'});
+            });
+        }
+    }).catch(function (err) {
         console.log(err);
         res.send({success: 'error'});
     });
--- auth/strategies/local/student.js
+++ auth/strategies/local/student.js
@@ -7,23 +7,29 @@ module.exports = new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password'
 }, function (email, password, done) {
-    passutils.pass2hash(password).then(function (hash) {
-        models.StudentLocal.findOne({
-            where: {email: email},
-            include: models.Student
-        }).then(function (studentlocal) {
-            if (!studentlocal)
-                return done(null, false, {message: 'Incorrect email'});
-            if (studentlocal.password === hash) {
-                return done(null, studentlocal.student);
-            } else {
-                return done(null, false, {message: 'Incorrect password'});
-            }
-        }).catch(function (err) {
-            console.log(err);
-            return done(err, false, {message: 'invalid user'});
-        });
-    }).catch(function () {
+
+    models.StudentLocal.findOne({
+        where: {email: email},
+        include: models.Student
+    }).then(function (studentLocal) {
+        console.log(studentLocal);
+        if (!studentLocal) {
+            return done(null, false, {message: 'Incorrect email'});
+        } else {
+            password.compare2hash(password, studentLocal.password).then(function (match) {
+                if (match) {
+                    console.log(2);
+                    return done(null, studentLocal.student);
+                } else {
+                    console.log(3);
+                    return done(null, false, {message: 'Incorrect password'});
+                }
+            }).catch(function (err) {
+                console.log(err);
+                return done(err, false, {message: 'invalid user'});
+            });
+        }
+    }).catch(function (err) {
         console.log(err);
         res.send({success: 'error'});
     });
