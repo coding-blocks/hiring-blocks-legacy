a61b965d55f6f4b7af3a8511ba7af620966c06a9
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2017-06-22 22:53:15 +0530

    Update api-index

--- routes/api/index.js
+++ routes/api/index.js
@@ -8,5 +8,6 @@ router.use('/companymanager', require('./companyManager'));
 router.use('/admin', require('./admin'));
 router.use('/companies', require('./companies'));
 router.use('/jobs', require('./jobs'));
+router.use('/users', require('./users'));
 
 module.exports = router;
---
+++ routes/api/users.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const models = require('./../../db/models').models;
+const password = require('./../../utils/password');
+
