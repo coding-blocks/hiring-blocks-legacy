608ac56c81ed057d0eb5a068a1e921e8b3085eef
apoorvaagupta <apoorvaagupta01@gmail.com>
2017-08-15 19:17:26 +0530

    refactored the jobs router
    Signed-off-by: apoorvaagupta <apoorvaagupta01@gmail.com>

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -1,24 +1,75 @@
 const router = require('express').Router();
 const models = require('./../../db/models').models;
+const password = require('./../../utils/password');
+const ensure = require('./../../auth/authutils');
+const passport = require('./../../auth/passporthandler');
 
-router.post('/add', function (req, res) {
+router.post('/add', passport.authenticate('bearer'),function (req, res) {
     const companyId=parseInt(req.body.companyId);
-    models.Job.create({
-        title: req.body.title,
-        description: req.body.description,
-        skills: req.body.skills,
-        jobType: req.body.jobType,
-        location: req.body.location,
-        stipend: req.body.stipend,
-        active: req.body.active,
-        startDate: req.body.startDate,
-        endDate: req.body.endDate,
-        companyId:companyId
-    }).then(function (job) {
-        res.status(201).send(job);
+
+  if (req.user) {
+    models.Admin.findOne({
+      where: {userId: req.user.id}
+    }).then(function (admin) {
+      if (admin) {
+        models.Job.create({
+          title: req.body.title,
+          description: req.body.description,
+          skills: req.body.skills,
+          jobType: req.body.jobType,
+          location: req.body.location,
+          stipend: req.body.stipend,
+          active: req.body.active,
+          startDate: req.body.startDate,
+          endDate: req.body.endDate,
+          companyId:companyId
+        }).then(function (job) {
+          res.status(201).send(job);
+        }).catch(function (err) {
+          console.log(err);
+          res.status(500).send("Could not add the job")
+        })
+
+      }
+      else {
+        models.CompanyManager.findOne({
+          where: {userId: req.user.id, companyId: companyId}
+        }).then(function (manager) {
+          if (manager) {
+            models.Job.create({
+              title: req.body.title,
+              description: req.body.description,
+              skills: req.body.skills,
+              jobType: req.body.jobType,
+              location: req.body.location,
+              stipend: req.body.stipend,
+              active: req.body.active,
+              startDate: req.body.startDate,
+              endDate: req.body.endDate,
+              companyId:companyId
+            }).then(function (job) {
+              res.status(201).send(job);
+            }).catch(function (err) {
+              console.log(err);
+              res.status(500).send("Could not add the job")
+            })
+
+          }
+          else
+            res.status(401).send("Only Admins and Company Managers Allowed");
+        }).catch(function (err) {
+          console.log(err);
+          res.status(500).send("Error");
+        })
+      }
     }).catch(function (err) {
-        console.log(err);
+      console.log(err);
+      res.status(500).send("Error");
     })
+  } else {
+    res.status(401).send("Please login first");
+  }
+
 });
 
 router.get('/:id', function (req, res) {
@@ -32,14 +83,13 @@ router.get('/:id', function (req, res) {
     })
 });
 
-router.post('/:id/apply', function (req, res) {
+router.post('/:id/apply', passport.authenticate('bearer'),ensure.ensureStudent,function (req, res) {
     let jobId = parseInt(req.params.id);
-    models.User.findOne({where : {id: req.query.userId}}).then(function (user) {
-        if(user.role === "Student"){
+
             models.Application.create({
                 application: req.body.application,
                 status: "none",
-                userId: req.query.userId,
+                userId: req.user.id,
                 jobId: jobId
             }).then(function (application) {
                 res.status(201).send(application)
@@ -47,11 +97,6 @@ router.post('/:id/apply', function (req, res) {
                 console.log(err);
                 res.status(500).send("Error in submitting the application")
             })
-        }else{
-            res.status(403).send("Only Students can submit the Applications");
-        }
-    })
-
 });
 
 router.get('/', function (req, res) {
