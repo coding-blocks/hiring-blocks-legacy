fd8f91e1a39d5f870b170e6d5d988c271ce9487c
apoorvaagupta <apoorvaagupta01@gmail.com>
2017-08-15 20:29:02 +0530

    router to get all apllications for a job
    Signed-off-by: apoorvaagupta <apoorvaagupta01@gmail.com>

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -99,6 +99,57 @@ router.post('/:id/apply', passport.authenticate('bearer'),ensure.ensureStudent,f
             })
 });
 
+router.get('/:id/applications', passport.authenticate('bearer'), function (req, res) {
+  let jobId = parseInt(req.params.id);
+  models.Admin.findOne({where: {id: req.user.id}}).then(function (admin) {
+    if (admin) {
+      models.Application.findAll({
+        where: {jobId: jobId},
+        include: models.Job
+      }).then(function (applications) {
+        if (!applications) {
+          return res.status(404).send({code: "404", error: {message: "No Applications submitted"}})
+        }
+        return res.status(200).send(applications);
+      }).catch(function (error) {
+        console.log(error);
+        res.status(500).send({code: "500", error: {message: "Database Error"}});
+      })
+    } else {
+      models.Job.findOne({where: {jobId: jobId} }).then(function (job) {
+        if(job){
+          models.CompanyManager.findOne({where: {userId: req.user.id, companyId: job.comapnyId}}).then(function (companymanager) {
+            if (companymanager) {
+              models.Application.findAll({
+                where: {jobId: jobId},
+                include: models.Job
+              }).then(function (applications) {
+                if (!applications) {
+                  return res.status(404).send({code: "404", error: {message: "No Applications submitted"}})
+                }
+                return res.status(200).send(applications);
+              }).catch(function (error) {
+                console.log(error);
+                res.status(500).send({code: "500", error: {message: "Database Error"}});
+              })
+            } else {
+              res.status(401).send({code: "401", error: {message: "Only companymanagers of this job's company are allowed"}});
+            }
+          }).catch(function (error) {
+            console.log(error);
+            res.status(500).send({code: "500", error: {message: "Database Error"}});
+          })
+        }else{
+          res.status(401).send({code: "401", error: {message: "this job does not exist"}});
+        }
+      }).catch(function (err) {
+        console.log(err)
+      })
+
+    }
+  })
+});
+
 router.get('/', function (req, res) {
     models.Job.findAll({
         where: {jobType: req.query.status}
