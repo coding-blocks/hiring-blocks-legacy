3c5360de5818a5b390ae3a547d2fb496c9782a0b
Shreya <shreyateeza@gmail.com>
2018-05-14 23:48:41 +0530

    Sends error in the form of JSON, solves #13.

--- test/api/index.js
+++ test/api/index.js
@@ -3,6 +3,7 @@ const mocha = require('mocha')
   , chaiHttp = require('chai-http')
   , expect = chai.expect
   , should = chai.should();
+var appjson = require('../../app.json');
 
 chai.use(chaiHttp)
 const api = chai.request("http://localhost:8000/api")
@@ -10,7 +11,7 @@ const api = chai.request("http://localhost:8000/api")
 it("Status code is 404", (done) => {
 
   api.get('/').end((err, res) => {
-    res.statusCode.should.equal(404)
+    res.statusCode.should.equal(appjson.error404.code)
     res.text.should.contain('Cannot GET /api/')
     done()
   })
